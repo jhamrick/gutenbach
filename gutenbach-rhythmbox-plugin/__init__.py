0159c0a25905932d89659336d73d160f46a6b9f1
Jessica B. Hamrick <jhamrick@mit.edu>
2009-11-29 17:59:45 -0500

    -Changed the name from pikamp3 to gutenbach. -Added comments. -Moved printername and hostname to constructor. -Created UI XML file for future preferences dialogue.

--- gutenbach-rhythmbox-plugin/__init__.py
+++ gutenbach-rhythmbox-plugin/__init__.py
@@ -4,43 +4,52 @@ import urlparse
 import urllib
 import subprocess
 
-menu_ui = """
+# UI Change is an extra item added to right-click menu when clicking on tracks
+popup_ui = """
 <ui> 
   <popup name="BrowserSourceViewPopup"> 
-    <menuitem name="Pikamp3" action="SendToPikamp3"/>
+    <menuitem name="Gutenbach" action="SendToGutenbach"/>
   </popup>
 </ui>
 """
-class Pikamp3Plugin(rb.Plugin):
+class GutenbachPlugin(rb.Plugin):
     def __init__(self):
         rb.Plugin.__init__(self)
 
     def activate(self, shell):
         self.shell = shell
-        # Create action for sending a track to pikamp3
-        action = gtk.Action('SendToPikamp3', _('Send to pikamp3'),
-                _("Queue selected tracks to the pikamp3 server."),
+        # Create action for sending a track to gutenbach
+        action = gtk.Action('SendToGutenbach', _('Send to gutenbach'),
+                _("Queue selected tracks to the gutenbach server."),
                 "")
-        action.connect('activate', self.pikamp3_action, shell)
-        action_group = gtk.ActionGroup('Pikamp3ActionGroup')
+        action.connect('activate', self.gutenbach_action, shell)
+        action_group = gtk.ActionGroup('GutenbachActionGroup')
         action_group.add_action(action)
         manager = shell.get_ui_manager()
         manager.insert_action_group(action_group)
-        manager.add_ui_from_string(menu_ui)
+        manager.add_ui_from_string(popup_ui)
+
+        # Default configuration options
+        self.printer = "pikamp3"
+        self.printer_host = "lbsg.mit.edu"
 
     def deactivate(self, shell):
         del self.shell
 
-    def pikamp3_action(self, action, shell):
+    def gutenbach_action(self, action, shell):
         source = shell.get_property("selected-source")
+        # For each track currently selected in the song browser
         for entry in source.get_entry_view().get_selected_entries():
+            # Only play files that are stored on the user's computer
             uri = entry.get_playback_uri()
             p = urlparse.urlparse(urllib.unquote(uri))
             if p.scheme == "file":
                 path = p.path
-                command = 'lpr -P pikamp3@lbsg.mit.edu "%s"' % path
+                if self.printer_host:
+                    printer = '@'.join([self.printer, self.printer_host])
+                else:
+                    printer = self.printer
+                command = 'lpr -P %s "%s"' % (printer, path)
                 print command
                 subprocess.Popen(command, shell=True)
-
-
 
