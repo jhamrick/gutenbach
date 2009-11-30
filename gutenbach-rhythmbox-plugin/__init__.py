61c50434af4f740357ebd00742384ea7fddd18d3
Nimrod Gileadi <nimrodg@localtoast.(none)>
2009-11-29 22:14:51 -0500

    Added docstrings. Beautified debugging output slightly.

--- gutenbach-rhythmbox-plugin/__init__.py
+++ gutenbach-rhythmbox-plugin/__init__.py
@@ -1,8 +1,15 @@
-import rb
+"""
+This module implements a Rhythmbox plugin that sends selected track to a
+printer queue. It's meant to be used with gutenbach, a music spooling printer
+server.
+
+The plugin uses lpr to send the files to gutenbach.
+"""
 import gtk
-import urlparse
-import urllib
+import rb
 import subprocess
+import urllib
+import urlparse
 
 # UI Change is an extra item added to right-click menu when clicking on tracks
 popup_ui = """
@@ -17,6 +24,9 @@ class GutenbachPlugin(rb.Plugin):
         rb.Plugin.__init__(self)
 
     def activate(self, shell):
+        """Adds a new item to the right-click menu which allows
+        sending music files to the gutenbach server.
+        """
         self.shell = shell
         # Create action for sending a track to gutenbach
         action = gtk.Action('SendToGutenbach', _('Send to gutenbach'),
@@ -37,6 +47,10 @@ class GutenbachPlugin(rb.Plugin):
         del self.shell
 
     def gutenbach_action(self, action, shell):
+        """An action called when the user clicks the right-click menu item
+        to send a track to gutenbach.
+        This function calls an instance of lpr for each file to be added.
+        """
         source = shell.get_property("selected-source")
         # For each track currently selected in the song browser
         for entry in source.get_entry_view().get_selected_entries():
@@ -50,6 +64,6 @@ class GutenbachPlugin(rb.Plugin):
                 else:
                     printer = self.printer
                 command = 'lpr -P %s "%s"' % (printer, path)
-                print command
+                print "About to run command '%s'" % command
                 subprocess.Popen(command, shell=True)
 
