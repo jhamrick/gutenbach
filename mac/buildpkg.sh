d88143c9aad71920a93e1b6f1e3ae9f1d8351830
rht <rudyht@rht-macbook.local>
2010-09-12 03:25:28 -0400

    simplify install scripts

--- mac/buildpkg.sh
+++ mac/buildpkg.sh
@@ -3,5 +3,6 @@
 cd ..
 make DESTDIR=../mac/tmp/ install
 
-/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -r mac/tmp/ -o mac/gutenbach.mpkg -i com.sipb -k -s mac/script -e mac/resources
+/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -r mac/tmp/ -o mac/gutenbach.mpkg -i com.sipb -k -e mac/resources
 
+rm -r mac/tmp
