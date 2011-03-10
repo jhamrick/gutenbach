a76f440eec227e17f00c52aec82eb82fbf788564
Quentin Smith <quentin@mit.edu>
2011-03-10 01:28:20 -0500

    ippattributegroup is a mix between a list and a dict (sort of an ordereddict, but the keys are actually the attributes' names

--- server/lib/ippattributegroup.py
+++ server/lib/ippattributegroup.py
@@ -6,10 +6,13 @@ from ippattribute import Attribute
 # initialize logger
 logger = logging.getLogger("ippLogger")
 
-class AttributeGroup():
+class AttributeGroup(object):
     """
     An AttributeGroup consists of an attribute-group-tag, followed by
-    a sequence of Attributes.
+    a sequence of Attributes. According to RFC 2565, "Within an
+    attribute-sequence, if two attributes have the same name, the
+    first occurrence MUST be ignored.", so we can effectively treat
+    this as an ordered dictionary.
     """
 
     def __init__(self, attribute_group_tag=None, attributes=[]):
@@ -34,36 +37,43 @@ class AttributeGroup():
                    "attribute_group_tag must be a character!"
             
 
-        if len(attributes) > 0:
-            for a in attributes:
-                assert isinstance(a, Attribute), \
-                       "attribute must be of type Attribute!"
-
         self.attribute_group_tag = attribute_group_tag
-        self.attributes = attributes
+        self.attributes[]
+        self.extend(attributes)
 
-    def getAttribute(self, name):
+    def __getitem__(self, name):
         """
         Returns a list of attributes which have name 'name'.
         """
         
-        return filter(lambda x: x.name == name, self.attributes)
+        attribute = filter(lambda x: x.name == name, self.attributes)
+        if attribute:
+            return attribute[0]
+        else:
+            raise KeyError("Attribute %r not found" % name)
 
-    def getTag(self):
+    def __contains__(self, name):
+        return len(filter(lambda x: x.name == name, self.attributes))
+
+    def __iter__(self):
+        return (a.name for a in self.attributes)
+    iterkeys = __iter__
+
+    def __setitem__(self, name, attribute):
         """
-        Return the attribute group tag.
+        Sets an attribute in the attribute group. Note that the key is
+        ignored and the attribute is queried for its name.
         """
 
-        return self.attribute_group_tag
+        return self.append(attribute)
 
-    def getAttributes(self):
-        """
-        Return the list of attributes in this attribue group.
-        """
+    def __delitem__(self, name):
+        self.attributes = filter(lambda x: x.name != name, self.attributes)
 
-        return self.attributes
+    def append(self, attribute):
+        return self.extend([attribute])
 
-    def setAttributes(self, attributes):
+    def extend(self, attributes):
         """
         Sets the attributes for the attribute group.
         """
@@ -72,30 +82,14 @@ class AttributeGroup():
             assert isinstance(a, Attribute), \
                    "attribute must be of type Attribute!"
 
-        self.attributes = attributes
+        for a in attributes:
+            # XXX: Instead of replacing the attribute, do we want to
+            # append the value to the attribute here?
+            del self[a.name]
+            self.attributes.append(a)
 
-    def addAttribute(self, attribute):
-        """
-        Adds an attribute to the list of attributes for the attribute
-        group.
-        """
-        
-        assert isinstance(attribute, Attribute), \
-               "attribute must be of type Attribute!"
-
-        self.attributes.append(attribute)
-
-    def setTag(self, tag):
-        """
-        Sets the attribute group tag.
-        """
-
-        assert isinstance(tag, char), \
-               "attribute tag must be a character!"
-
-        self.attribute_group_tag = tag
-
-    def pack(self):
+    @property
+    def packed_value(self):
         """
         Convert the AttributeGroup to binary.
         """
@@ -104,7 +98,7 @@ class AttributeGroup():
         tag = struct.pack('>b', self.attribute_group_tag)
 
         # convert each of the attributes to binary
-        attributes = [a.toBinaryData() for a in self.attributes]
+        attributes = [a.packed_value for a in self.attributes]
 
         # concatenate everything and return
         return tag + ''.join(attributes)
