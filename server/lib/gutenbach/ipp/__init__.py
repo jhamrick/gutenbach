08a764a014e58fe90d9bf0e79791e604272da16e
Jessica B. Hamrick <jhamrick@mit.edu>
2011-12-20 11:42:41 -0500

    Add operations as 'ops' in ipp/__init__.py

--- server/lib/gutenbach/ipp/__init__.py
+++ server/lib/gutenbach/ipp/__init__.py
@@ -3,3 +3,6 @@ from attributegroup import AttributeGroup
 from request import Request
 from value import Value
 from constants import Tags
+import operations as ops
+
+__all__ = ['Attribute', 'AttributeGroup', 'Request', 'Value', 'Tags', 'ops']
